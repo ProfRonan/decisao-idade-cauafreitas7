9e6e6fec0fc02a9b199070f39e869ece11e723c6
cauafreitas7 <128155396+cauafreitas7@users.noreply.github.com>
2023-05-24 14:06:46 +0000

    agr foi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ número = input('Digite um número: ')
 número = int(número)
 if número < 0:
    print('Impossível!')
-elif número > 0 and número < 18:
+elif número >= 0 and número < 18:
    print('Não precisa se alistar.')
 elif número > 18 and número < 65:
    print('Não esqueça de votar na próxima eleição.')
